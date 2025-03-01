7a618fca6ce1ae8de73930d23b4ba962486f09a2
IdanTraub <idantraub@gmail.com>
2025-03-02 00:39:54 +0200

    Add input elements to UI without styling

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const getColorBtn= document.getElementById("get-Color-btn")
+getColorBtn.addEventListener("click",handleColorBtnClick)
+
+async function handleColorBtnClick()
+{
+    const color = document.getElementById("color-selector").value
+    const theme = document.getElementById("theme").value
+    
+
+}
