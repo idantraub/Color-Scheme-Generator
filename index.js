db05991c638e7414e4cfa80cb0d06675c743a589
IdanTraub <idantraub@gmail.com>
2025-03-02 00:55:54 +0200

    getting a response that is meaningful

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ getColorBtn.addEventListener("click",handleColorBtnClick)
 async function handleColorBtnClick()
 {
     const color = document.getElementById("color-selector").value
-    const theme = document.getElementById("theme").value
+    const mode = document.getElementById("theme").value
     
+    console.log(color," ",mode)
+    const url = `https://www.thecolorapi.com/scheme?hex=${color.slice(1)}&mode=${mode}&count=5`
+    console.log(url)
+    fetch(url)
+    .then(response=>response.json()).then(data=>console.log(data))
 
 }
